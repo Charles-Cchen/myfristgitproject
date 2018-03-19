21459b1139f4a5554fd281c15496002990130c63
Charles-Cchen <920095705@qq.com>
2018-03-19 15:50:42 +0800

    第一次提交版本修改内容

--- src/test/java/com/example/demo/controller/Ai1Test.java
+++ src/test/java/com/example/demo/controller/Ai1Test.java
@@ -10,6 +10,7 @@ public class Ai1Test {
         String[] str = {"ha","bb","cc"};
         ai.show(str);
         String[] str1 = {"hah","bb","cc"};
+        String message = "版本提交第一次";
         ai.show(str1);
     }
 }
